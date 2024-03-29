c800c66a08d8cfdfd1365567b131e4bd87e5aec1
nikos600 <sdi1900306@di.uoa.gr>
2024-03-29 11:21:03 +0200

    fdd change

--- fd1.c
+++ fd1.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+		
+		 typedef char AirportCode[4];
+ 			typedef struct NodeTag {
+ 				AirportCode Airport;
+ 				struct NodeTag *Link;
+			} NodeType;
+ 		typedef NodeType *NodePointer;
+
 
 
 	int main(void) {
+
+		 AirportCode C;
+ 		NodePointer L;
+	    strcpy(C, "BRU");
+ 		printf("%s\n", C);
+ 		L=(NodePointer)malloc(sizeof(NodeType));
+ 		strcpy(L->Airport, C);
+ 		printf("%s \n", L->Airport);
 		return 0;
 	}
